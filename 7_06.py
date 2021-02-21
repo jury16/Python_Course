e6c85a727cafb8876a754073d50706120cfe8adf
Yurii <76671367+jury16@users.noreply.github.com>
2021-02-21 18:02:09 -0500

    Upload fixed file

--- 7_06.py
+++ 7_06.py
@@ -6,17 +6,17 @@
 """
 def sum_elements(*args):
     sum = 0
-    maxx = int(args[0][0])
-    for i in (args[0]):
+    maxx = args[0]
+    for i in args:
         sum += int(i)
         if int(i) > maxx:
             maxx = i
 
-    return sum, int(maxx)
+    return f'sum_of_elements = {sum} \nmax_element = {maxx}'
 
 s_list = input('Input arguments (use space): ')
-list_arguments = tuple(s_list.split())
-print(sum_elements(list_arguments))
+list_arguments = list(map(int,s_list.split()))
+print(sum_elements(*list_arguments))
 
 
 
